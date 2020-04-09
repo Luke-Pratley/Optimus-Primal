25ca92d99751d1c4ad7d381b29246b2b5d6d90ba
Luke Pratley <Luke.Pratley@gmail.com>
2020-04-09 18:45:09 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="optimiusprimal",
+    version="0.0.1",
+    author_email="luke.pratley@gmail.com",
+    description="Convex Optimization Primal Dual Solver",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Luke-Pratley/optimusprimal",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: GNU GPL v3.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
