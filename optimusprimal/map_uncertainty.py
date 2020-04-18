4cf0f209e392eb8cbb33f0cf4b176194029dd06b
Luke Pratley <Luke.Pratley@gmail.com>
2020-04-18 19:56:04 -0400

    added and fixed test for uq

--- optimusprimal/map_uncertainty.py
+++ optimusprimal/map_uncertainty.py
@@ -1,17 +1,16 @@
 import numpy as np
 
 
-def bisection_method_credible_interval(x_sol, region, objective_function, bound, iters, tol):
+def bisection_method_credible_interval(x_sol, region, objective_function, start_interval, bound, iters, tol):
     """
     Bisection method for finding credible interval
     """
 
-    eta1 = 0
-    eta2 = 10
-    x = x_sol
+    eta1 = start_interval[0]
+    eta2 = start_interval[1]
+    x = np.copy(x_sol)
     obj3 = objective_function(x_sol) - bound
     for i in range(int(iters)):
-        print(eta1, eta2, obj3)
         x[region] = 0
         x[region] += eta1
         obj1 = objective_function(x) - bound
@@ -25,3 +24,20 @@ def bisection_method_credible_interval(x_sol, region, objective_function, bound,
             eta1 = eta3
         else:
             eta2 = eta3
+    return np.nan
+
+
+def create_credible_region(x_sol, region_size, objective_function, bound, iters, tol, top, bottom):
+    region = np.zeros(x_sol.shape, dtype=bool)
+    region[:,:] = False
+    region[:region_size, :region_size] = True
+    dsizey, dsizex = int(x_sol.shape[0]/region_size), int(x_sol.shape[1]/region_size)
+    error_p = np.zeros((dsizey, dsizex))
+    error_m = np.zeros((dsizey, dsizex))
+    for i in range(dsizey):
+        for j in range(dsizex):
+            mask = np.roll(region, shift=(i * region_size, j * region_size))
+            x_mean = np.mean(np.ravel(x_sol[(mask)]))
+            error_p[i, j] = bisection_method_credible_interval(x_sol, (mask), objective_function, [x_mean, top], bound, iters, tol)
+            error_m[i, j] = bisection_method_credible_interval(x_sol, (mask), objective_function, [bottom, x_mean], bound, iters, tol)
+    return error_p, error_m
--- tests/uq_tests.py
+++ tests/uq_tests.py
@@ -1,23 +1,40 @@
 import optimusprimal.map_uncertainty as map_uncertainty
 import pytest
-from numpy import linalg as LA
 import numpy as np
 
 def test_bisection():
     x_sol = np.zeros((16, 16))
     obj = lambda x: np.sum(x**2)
     region = np.ones(x_sol.shape, dtype=bool)
-    val = map_uncertainty.bisection_method_credible_interval(x_sol, region, obj, 9 * 16 * 16, 1e3, 1e-3)
+    val = map_uncertainty.bisection_method_credible_interval(x_sol, region, obj, [0, 4], 9 * 16 * 16, 1e3, 1e-3)
     assert np.allclose(val, 3., 1e-2)
 
     x_sol = np.zeros((128, 128))
     obj = lambda x: np.sum(x**2)
     region = np.ones(x_sol.shape, dtype=bool)
-    val = map_uncertainty.bisection_method_credible_interval(x_sol, region, obj, 9 * 128 * 128, 1e3, 1e-3)
+    val = map_uncertainty.bisection_method_credible_interval(x_sol, region, obj, [0, 4], 9 * 128 * 128, 1e3, 1e-3)
     assert np.allclose(val, 3., 1e-2)
 
     x_sol = np.zeros((128, 128))
     obj = lambda x:  np.sum(x**2)
     region = np.ones(x_sol.shape, dtype=bool)
-    val = map_uncertainty.bisection_method_credible_interval(x_sol, region, obj, 16 * 128 * 128, 1e3, 1e-3)
+    val = map_uncertainty.bisection_method_credible_interval(x_sol, region, obj, [0, 4], 16 * 128 * 128, 1e3, 1e-3)
     assert np.allclose(val, 4., 1e-2)
+
+    x_sol = np.zeros((128, 128))
+    obj = lambda x:  np.sum(x**2)
+    region = np.ones(x_sol.shape, dtype=bool)
+    val = map_uncertainty.bisection_method_credible_interval(x_sol, region, obj, [-4, 0], 16 * 128 * 128, 1e3, 1e-3)
+    assert np.allclose(val, -4., 1e-2)
+
+    x_sol = np.zeros((128, 128))
+    region_size = 16
+    obj = lambda x:  np.sum(np.abs(x)**2)/ (region_size * region_size)
+    bound =  4
+    iters = 1e2
+    tol = 1e-3
+    error_p, error_m = map_uncertainty.create_credible_region(x_sol, region_size, obj, bound, iters, tol, 3, -3)
+    assert np.allclose(error_p.shape, (128/region_size, 128/region_size), 1e-3)
+    assert np.allclose(error_m.shape, (128/region_size, 128/region_size), 1e-3)
+    assert np.allclose(error_p, 2., 1e-2)
+    assert np.allclose(error_m, -2., 1e-2)
