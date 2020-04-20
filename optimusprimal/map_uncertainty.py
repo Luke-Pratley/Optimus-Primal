c2d378c8b605ded7dda0cc179d9f7909bfa9c144
Luke Pratley <Luke.Pratley@gmail.com>
2020-04-20 00:32:54 -0400

    fixed tests for uq

--- optimusprimal/map_uncertainty.py
+++ optimusprimal/map_uncertainty.py
@@ -1,43 +1,67 @@
 import numpy as np
 
 
-def bisection_method_credible_interval(x_sol, region, objective_function, start_interval, bound, iters, tol):
+def bisection_method_credible_interval(objective_function, start_interval, iters, tol):
     """
     Bisection method for finding credible interval
     """
 
     eta1 = start_interval[0]
     eta2 = start_interval[1]
-    x = np.copy(x_sol)
-    obj3 = objective_function(x_sol) - bound
+    obj3 = objective_function(eta2)
+    if(np.sign(objective_function(eta1)) == np.sign(objective_function(eta2))):
+        print("eta1 and eta2 have same sign.")
     for i in range(int(iters)):
-        x[region] = 0
-        x[region] += eta1
-        obj1 = objective_function(x) - bound
+        obj1 = objective_function(eta1)
         eta3 = (eta2 + eta1) * 0.5
-        x[region] = 0
-        x[region] += eta3
-        obj3 = objective_function(x) - bound
-        if(np.abs(obj3) < tol):
+        obj3 = objective_function(eta3)
+        if(np.abs(obj3) < tol or np.abs(obj1) < tol):
+            return eta3
+        if(np.abs(eta1 - eta2) < 1e-12):
             return eta3
         if(np.sign(obj1) == np.sign(obj3)):
             eta1 = eta3
         else:
             eta2 = eta3
-    return np.nan
+    print("Did not converge... ",obj3)
+    return eta3
 
 
-def create_credible_region(x_sol, region_size, objective_function, bound, iters, tol, top, bottom):
-    region = np.zeros(x_sol.shape, dtype=bool)
-    region[:,:] = False
-    region[:region_size, :region_size] = True
-    dsizey, dsizex = int(x_sol.shape[0]/region_size), int(x_sol.shape[1]/region_size)
-    error_p = np.zeros((dsizey, dsizex))
-    error_m = np.zeros((dsizey, dsizex))
-    for i in range(dsizey):
-        for j in range(dsizex):
-            mask = np.roll(region, shift=(i * region_size, j * region_size))
-            x_mean = np.mean(np.ravel(x_sol[(mask)]))
-            error_p[i, j] = bisection_method_credible_interval(x_sol, (mask), objective_function, [x_mean, top], bound, iters, tol)
-            error_m[i, j] = bisection_method_credible_interval(x_sol, (mask), objective_function, [bottom, x_mean], bound, iters, tol)
-    return error_p, error_m
+def create_credible_region(x_sol, region_size, objective_function, bound, iters, tol, top):
+    """
+    Bisection method for finding credible interval
+    """
+
+    region = np.zeros(x_sol.shape)
+    if len(x_sol.shape) > 1:
+        region[:region_size, :region_size] = 1.
+        dsizey, dsizex = int(x_sol.shape[0]/region_size), int(x_sol.shape[1]/region_size)
+        error_p = np.zeros((dsizey, dsizex))
+        error_m = np.zeros((dsizey, dsizex))
+        mean = np.zeros((dsizey, dsizex))
+        for i in range(dsizey):
+            for j in range(dsizex):
+                mask = np.roll(np.roll(region, shift=i * region_size, axis=0), shift=j * region_size, axis=1)
+                x_mean = np.mean(np.ravel(x_sol[(mask.astype(bool))]))
+                mean[i, j] = x_mean
+                obj = lambda eta : objective_function(x_sol * (1. - mask) + eta * mask) - bound
+                error_p[i, j] = bisection_method_credible_interval(obj, [0, top], iters, tol)
+                obj = lambda eta : objective_function(x_sol * (1. - mask) - eta * mask ) - bound
+                error_m[i, j] = -bisection_method_credible_interval(obj, [0, top], iters, tol)
+                print(i, j, (error_p[i, j], error_m[i, j]), x_mean)
+    else:
+        region[:region_size] = 1.
+        dsizey = int(x_sol.shape[0]/region_size)
+        error_p = np.zeros((dsizey))
+        error_m = np.zeros((dsizey))
+        mean = np.zeros((dsizey))
+        for i in range(dsizey):
+                mask = np.roll(region, shift=i * region_size, axis=0)
+                x_mean = np.mean(np.ravel(x_sol[(mask.astype(bool))]))
+                mean[i] = x_mean
+                obj = lambda eta : objective_function(x_sol * (1. - mask) + eta * mask) - bound
+                error_p[i] = bisection_method_credible_interval(obj, [0, top], iters, tol)
+                obj = lambda eta : objective_function(x_sol * (1. - mask) - eta * mask) - bound
+                error_m[i] = -bisection_method_credible_interval(obj, [0, top], iters, tol)
+                print(i, (error_p[i], error_m[i]), x_mean)
+    return error_p, error_m, mean
--- tests/uq_tests.py
+++ tests/uq_tests.py
@@ -3,38 +3,34 @@ import pytest
 import numpy as np
 
 def test_bisection():
-    x_sol = np.zeros((16, 16))
-    obj = lambda x: np.sum(x**2)
-    region = np.ones(x_sol.shape, dtype=bool)
-    val = map_uncertainty.bisection_method_credible_interval(x_sol, region, obj, [0, 4], 9 * 16 * 16, 1e3, 1e-3)
+    obj = lambda x: np.sum(x**2) - 9
+    val = map_uncertainty.bisection_method_credible_interval(obj, [0, 10.] , 1e3, 1e-3)
     assert np.allclose(val, 3., 1e-2)
 
-    x_sol = np.zeros((128, 128))
-    obj = lambda x: np.sum(x**2)
-    region = np.ones(x_sol.shape, dtype=bool)
-    val = map_uncertainty.bisection_method_credible_interval(x_sol, region, obj, [0, 4], 9 * 128 * 128, 1e3, 1e-3)
-    assert np.allclose(val, 3., 1e-2)
-
-    x_sol = np.zeros((128, 128))
-    obj = lambda x:  np.sum(x**2)
-    region = np.ones(x_sol.shape, dtype=bool)
-    val = map_uncertainty.bisection_method_credible_interval(x_sol, region, obj, [0, 4], 16 * 128 * 128, 1e3, 1e-3)
-    assert np.allclose(val, 4., 1e-2)
-
-    x_sol = np.zeros((128, 128))
-    obj = lambda x:  np.sum(x**2)
-    region = np.ones(x_sol.shape, dtype=bool)
-    val = map_uncertainty.bisection_method_credible_interval(x_sol, region, obj, [-4, 0], 16 * 128 * 128, 1e3, 1e-3)
-    assert np.allclose(val, -4., 1e-2)
+    obj = lambda x: np.sum(x**2) - 9
+    val = map_uncertainty.bisection_method_credible_interval(obj, [-10., 0] , 1e3, 1e-3)
+    assert np.allclose(val, -3., 1e-2)
 
     x_sol = np.zeros((128, 128))
     region_size = 16
-    obj = lambda x:  np.sum(np.abs(x)**2)/ (region_size * region_size)
-    bound =  4
-    iters = 1e2
-    tol = 1e-3
-    error_p, error_m = map_uncertainty.create_credible_region(x_sol, region_size, obj, bound, iters, tol, 3, -3)
+    obj = lambda x:  np.sum(np.abs(x)**2)
+    bound = 4.
+    iters = 1e5
+    tol = 1e-12
+    error_p, error_m, mean = map_uncertainty.create_credible_region(x_sol, region_size, obj, bound, iters, tol, 5)
     assert np.allclose(error_p.shape, (128/region_size, 128/region_size), 1e-3)
     assert np.allclose(error_m.shape, (128/region_size, 128/region_size), 1e-3)
-    assert np.allclose(error_p, 2., 1e-2)
-    assert np.allclose(error_m, -2., 1e-2)
+    assert np.allclose(error_p, 2./region_size, 1e-3)
+    assert np.allclose(error_m, -2./region_size, 1e-3)
+    assert np.allclose(mean, 0., 1e-12)
+    x_sol = np.zeros((128, ))
+    region_size = 16
+    obj = lambda x:  np.sum(np.abs(x)**2)
+    bound =  4
+    iters = 1e5
+    tol = 1e-12
+    error_p, error_m, mean = map_uncertainty.create_credible_region(x_sol, region_size, obj, bound, iters, tol, 5)
+    assert np.allclose(error_p.shape, (128/region_size, ), 1e-3)
+    assert np.allclose(error_m.shape, (128/region_size, ), 1e-3)
+    assert np.allclose(error_p, 2./np.sqrt(region_size), 1e-3)
+    assert np.allclose(error_m, -2./np.sqrt(region_size), 1e-3)
